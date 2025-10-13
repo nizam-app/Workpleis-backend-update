cef2aeda2b1ea8a97f7f199dce3fbf8ea5f0ba47
Palash Chandra Roy <90688529+Palash-Chandra-Roy@users.noreply.github.com>
2025-10-13 18:59:58 +0600

    update globalErrorHandler

--- utils/globalErrorHandler.js
+++ utils/globalErrorHandler.js
@@ -5,14 +5,34 @@ export const globalErrorHandle = (err  , req , res , next)=>{
     let statusCode = 500;
     let message = `Something went wrong!`;
 
-    if(err instanceof AppError){
+    //mongoose duplicate error
+    if(err.code === 11000){
+        statusCode = 400;
+        const duplicate = err.message.match(/"([^"]*)"/)[1];
+        message = `${duplicate} already exist!`
+    }
+    //mongoose CastError 
+    else if(err.name === 'CastError'){
+        statusCode = 400;
+        message = 'Invalid mongoDB object ID, Please provide valid ID.'
+    }
+    //mongoose ValidationError
+    else if(err.name === "ValidationError"){
+        statusCode = 400;
+        message = "Invalid Input"
+    }
+    // here will be add a zod error
+
+    // custom error 
+    else if(err instanceof AppError){
         statusCode = err.statusCode;
         message = err.message;
-    }else if(err instanceof Error){
+    }
+    // server error 
+    else if(err instanceof Error){
         statusCode = 500;
         message = err.message;
     }
-
     
     res.status(statusCode).json({
         status : 'Failed',
